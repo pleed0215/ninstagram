3c3152df1e36e35c2dbd3973b91ed80c8a4646cf
이은덕 <pleed0215@ieundeog-ui-MacBookPro.local>
2020-12-06 00:12:29 +0900

    footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Footer: React.FC = () => (
+  <footer className="w-full h-16 flex items-center justify-center">
+    <div>
+      <li className="list-none flex flex-row justify-center items-center">
+        <ul>
+          <a className="footer__list_item" href="#">
+            ABOUT US
+          </a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">SUPPORT</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">PRESS</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">API</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">JOBS</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">PRIVACY</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">TERMS</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">DRIRECTOY</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">PROFILES</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">HASHTAGS</a>
+        </ul>
+        <ul>
+          <a className="footer__list_item" href="#">LANGUAGE</a>
+        </ul>
+      </li>
+    </div>
+    <div className="ml-8">
+        <p className=" text-gray-400 text-sm font-normal ">&copy; 2020 NINSTAGRAM</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/Router/Auth.tsx
+++ src/Router/Auth.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import Footer from "../Components/Footer";
 
-const Auth: React.FC = () => (
-  <div>
-    <h1>Auth</h1>
-  </div>
-);
+const Auth: React.FC = () => {
+  const [action, setAction] = useState("logIn");
+  return (
+    <>
+      <div className="w-1/2 h-4/5 mx-auto flex items-center justify-center">
+        <h1 className="p-4 mx-auto">
+          {action === "logIn" ? "Login" : "SignUp"}
+        </h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Auth;
